f344d53e2f3a29c64fe129e473ec96c371798c67
t-furuta <takahisa.furuta@sparty.jp>
2023-07-19 10:47:46 +0900

    user周り

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Patch, Req, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { UserService } from './user.service';
+import { Request } from 'express';
+import { User } from '@prisma/client';
+import { UpdateUserDto } from './dto/update-user.dto';
 
+@UseGuards(AuthGuard('jwt'))
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  getLoginUser(@Req() req: Request): Omit<User, 'hashedPassword'> {
+    return req.user;
+  }
+
+  @Patch()
+  updateUser(@Req() req: Request, @Body() dto: UpdateUserDto) {
+    return this.userService.updateUser(req.user.id, dto);
+  }
+}
