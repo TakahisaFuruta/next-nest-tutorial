0dfd8cdf842b55384ae388198b042609dc3aa54a
t-furuta <takahisa.furuta@sparty.jp>
2023-07-19 10:55:46 +0900

    taskまわり

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateTaskDto } from './dto/create-todo.dto';
+import { Task } from '@prisma/client';
 
 @Injectable()
-export class TodoService {}
+export class TodoService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async createTodo(userId: number, dto: CreateTaskDto): Promise<Task> {
+    const task = this.prisma.task.create({
+      data: {
+        userId: userId,
+        ...dto,
+      },
+    });
+    return task;
+  }
+}
