2564c53dde298b45ff4fa0c2740169d1ed630600
t-furuta <takahisa.furuta@sparty.jp>
2023-07-20 10:06:18 +0900

    todo周りの処理

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,4 +1,63 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Req,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { TodoService } from './todo.service';
+import { Request } from 'express';
+import { CreateTaskDto } from './dto/create-todo.dto';
+import { Task } from '@prisma/client';
+import { UpdateTaskDto } from './dto/update-todo.dto';
 
+@UseGuards(AuthGuard('jwt'))
 @Controller('todo')
-export class TodoController {}
+export class TodoController {
+  constructor(private readonly todoService: TodoService) {}
+
+  @Get()
+  getTasks(@Req() req: Request): Promise<Task[]> {
+    return this.todoService.getTodoList(req.user.id);
+  }
+
+  @Get(':id')
+  getTask(
+    @Req() req: Request,
+    @Param('id', ParseIntPipe) taskId: number,
+  ): Promise<Task> {
+    return this.todoService.getTodo(req.user.id, taskId);
+  }
+
+  @Post()
+  create(@Req() req: Request, @Body() dto: CreateTaskDto): Promise<Task> {
+    return this.todoService.createTodo(req.user.id, dto);
+  }
+
+  @Patch(':id')
+  updateTask(
+    @Req() req: Request,
+    @Param('id', ParseIntPipe) taskId: number,
+    @Body() dto: UpdateTaskDto,
+  ) {
+    return this.todoService.updateTodo(req.user.id, taskId, dto);
+  }
+
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @Delete(':id')
+  deleteTask(
+    @Req() req: Request,
+    @Param('id', ParseIntPipe) taskId: number,
+  ): Promise<void> {
+    return this.todoService.deleteTask(req.user.id, taskId);
+  }
+}
--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -2,18 +2,63 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateTaskDto } from './dto/create-todo.dto';
 import { Task } from '@prisma/client';
+import { UpdateTaskDto } from './dto/update-todo.dto';
 
 @Injectable()
 export class TodoService {
   constructor(private readonly prisma: PrismaService) {}
 
+  async getTodoList(userId: number): Promise<Task[]> {
+    const tasks = await this.prisma.task.findMany({
+      where: {
+        userId: userId,
+      },
+    });
+    return tasks;
+  }
+
+  async getTodo(userId: number, taskId: number): Promise<Task> {
+    const task = await this.prisma.task.findFirst({
+      where: {
+        userId: userId,
+        id: taskId,
+      },
+    });
+    return task;
+  }
+
   async createTodo(userId: number, dto: CreateTaskDto): Promise<Task> {
-    const task = this.prisma.task.create({
+    const task = await this.prisma.task.create({
       data: {
         userId: userId,
         ...dto,
       },
     });
     return task;
+  }
+
+  async updateTodo(
+    userId: number,
+    taskId: number,
+    dto: UpdateTaskDto,
+  ): Promise<Task> {
+    return await this.prisma.task.update({
+      where: {
+        userId: userId,
+        id: taskId,
+      },
+      data: {
+        ...dto,
+      },
+    });
+  }
+
+  async deleteTask(userId: number, taskId: number): Promise<void> {
+    await this.prisma.task.delete({
+      where: {
+        userId: userId,
+        id: taskId,
+      },
+    });
   }
 }
